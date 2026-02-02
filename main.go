6e03152ef178784d2dd80da56aed2bb3f63b681c
Valentino <valentinokukurichkaapple@gmail.com>
2026-02-02 20:49:14 +0100

    the end of the project

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
+func main() {
+	jobs := make(chan int)
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		for i := 1; ; i++ {
+			time.Sleep(3 * time.Second)
+			jobs <- i
+		}
+	}()
+	for {
+		select {
+		case job := <-jobs:
+			fmt.Printf("Обработка задачи №%d\n", job)
+			time.Sleep(500 * time.Millisecond)
+			fmt.Println("Готово!")
+		case s := <-sigs:
+			fmt.Printf("\n Получен сигнал: %v\n", s)
+			time.Sleep(2 * time.Second)
+			fmt.Println("Все данные сохранены. Выход.")
+			return
+
+		case <-time.After(2 * time.Second):
+			// Это сработает, если в jobs ничего не приходило 2 секунды
+			fmt.Println("логов нет, ждем новых задач")
+		}
+	}
 }
